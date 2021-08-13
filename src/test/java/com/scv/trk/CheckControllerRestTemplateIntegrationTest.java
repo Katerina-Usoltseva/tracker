f08c68ba363b8311debf8b3860158578d47c15f3
Katerina Usoltseva <usoltsevaekaterinaa@gmail.com>
2021-08-14 00:55:13 +0700

    Added spring security for two roles

---
+++ src/test/java/com/scv/trk/CheckControllerRestTemplateIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.scv.trk;
+
+import com.scv.trk.model.CheckInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CheckControllerRestTemplateIntegrationTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void givenAuthRequestForUserRole_shouldSucceedWith200()
+        throws Exception {
+        ResponseEntity<String> result =
+                restTemplate.withBasicAuth("user", "userPass")
+                .getForEntity("/checks", String.class);
+
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+}
